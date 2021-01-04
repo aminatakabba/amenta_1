60c15488c989b49c1ca60e1e51ba3a7d1743d74c
Aminata Kabba <aminata_kabba@hotmail.com>
2021-01-04 22:05:08 +0000

    banner finished

--- amenta/src/Components/App/App.js
+++ amenta/src/Components/App/App.js
@@ -1,3 +1,4 @@
+import Banner from '../Banner/Banner';
 import Header from '../Header/Header';
 import './App.css'
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header /> 
+      <Banner />
     </div>
   );
 }
---
+++ amenta/src/Components/Banner/Banner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className="banner">
+            <div className="banner__Right">
+                <h1>front end web </h1>
+                <h1>and software </h1>
+                <h1>development </h1>
+                <h1>agency</h1>
+            </div>
+
+            <div className="banner_Left">
+                <div className="banner__LeftDiv">
+                    <div className="banner__LeftImg"></div>
+                    <h2>aminata kabba</h2>
+                    <p>behind the code</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
