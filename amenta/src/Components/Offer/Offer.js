8877fc4fbe29b818f894a5ee55e61c780be10fb0
Aminata Kabba <aminata_kabba@hotmail.com>
2021-01-12 23:14:46 +0000

    services section added

--- amenta/src/Components/Offer/Offer.js
+++ amenta/src/Components/Offer/Offer.js
@@ -1,18 +1,70 @@
 import React from 'react';
 import Offers from '../Offers/Offers';
 import './Offer.css'
-// import DevicesIcon from '@material-ui/icons/Devices';
+import DevicesIcon from '@material-ui/icons/Devices';
+import laptop from '../img/laptop.png'
+import device from '../img/device.png'
+import website from '../img/website.png'
+
 
 function Offer() {
     return (
         <div className="offer">
-           <Offers 
-                // img={DevicesIcon}
-                title="Web Developer" 
-                paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" 
-                className="offer__offers"/> 
+                <div className="offfer__line1">
+                    <div className="offer__one">
+                        <img src={laptop} className="offer__img"/>
+                        <Offers 
+                            title="Web Developer" 
+                            paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" 
+                            className="offer__offers"
+                        /> 
+                    </div>
+                    <div className="offer__one">
+                        <img src={device} className="offer__img"/>
+                        <Offers 
+                            title="Web Developer" 
+                            paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" 
+                            className="offer__offers"
+                        /> 
+                    </div>
+                    <div className="offer__one">
+                        <img src={website} className="offer__img"/>
+                        <Offers 
+                            title="Web Developer" 
+                            paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" 
+                            className="offer__offers"
+                        /> 
+                    </div>
+                </div>
+
+            <div className="offer__line2">
+                <div className="offer__one">
+                    <img src={laptop} className="offer__img"/>
+                    <Offers 
+                        title="Web Developer" 
+                        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" 
+                        className="offer__offers"
+                    /> 
+                </div>
+                <div className="offer__one">
+                    <img src={device} className="offer__img"/>
+                    <Offers 
+                        title="Web Developer" 
+                        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" 
+                        className="offer__offers"
+                    /> 
+                </div>
+                <div className="offer__one">
+                    <img src={website} className="offer__img"/>
+                    <Offers 
+                        title="Web Developer" 
+                        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" 
+                        className="offer__offers"
+                    /> 
+                </div>
+            </div>
         </div>
     )
 }
-
+// <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 export default Offer
--- amenta/src/Components/Offers/Offers.js
+++ amenta/src/Components/Offers/Offers.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Offers.css'
 import DevicesIcon from '@material-ui/icons/Devices';
 
-function Offers({ img, title, paragraph}) {
+function Offers({ image, title, paragraph}) {
     return (
         <div className="offers">
-            <DevicesIcon className="offers__icon"/>
-            {/* <p>{img}</p> */}
+            {/* <DevicesIcon className="offers__icon"/> */}
+            <p>{image}</p>
             <h1>{title}</h1>
             <p>{paragraph}</p>
         </div>
